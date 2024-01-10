c96b7e4ebb5580f62d648042b6648b453544ceb0
David Cavar <david.cavar@morescreens.net>
2024-01-10 13:31:20 +0100

    migrate to using context menus

--- src/js/new_service_worker.js
+++ src/js/new_service_worker.js
@@ -1,49 +1,77 @@
-/**
- * Modifications copyright (C) 2017 David Ćavar
- */
 var enabled = true;
 
 var extension_page = chrome.runtime.getURL('/index.html');
-var substitution = extension_page + "#\\0"
+// var substitution = extension_page + "#\\0"
 
-console.log(extension_page, substitution)
+// var rules = [{
+//   id: 1,
+//   action: {
+//     type: 'redirect',
+//     redirect: { regexSubstitution: substitution },
+//   },
+//   condition: {
+//     regexFilter: '^https?://.*/(.*\\.m3u8?|.*\\.mpd|Manifest).*',
+//     resourceTypes: ['main_frame', 'sub_frame'],
+//   },
+// }];
 
-var rules = [{
-  id: 1,
-  action: {
-    type: 'redirect',
-    redirect: { regexSubstitution: substitution },
-  },
-  condition: {
-    regexFilter: '^https?://.*/([a-zA-Z0-9-_]+)(\.mpd)|(\.m3u(8)?)|(/Manifest).*',
-    resourceTypes: ['main_frame', 'sub_frame'],
-  },
-}];
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request, sender, sendResponse);
-    
-    if (request == "getState") {
-        sendResponse(enabled);
-        return;
+function onClick(info) {
+    console.log(info);
+
+    if('link' == info.menuItemId) {
+        chrome.tabs.create({ 'url': extension_page + "#" + info.linkUrl }, function (tab) { });
     }
-      
-    enabled = request == "ENABLE";
-    enabled ? chrome.action.setIcon({path: "assets/icon128.png" }) : chrome.action.setIcon({ path:"assets/icon128grey.png" });
 
-    if(true === enabled) {
-        chrome.declarativeNetRequest.updateDynamicRules({
-            removeRuleIds: rules.map(r => r.id),
-            addRules: rules,
-        });
-    } else {
-        chrome.declarativeNetRequest.updateDynamicRules(chrome.declarativeNetRequest.updateDynamicRules({
-            removeRuleIds: rules.map(r => r.id)
-        }));
+    if('link-text' == info.menuItemId) {
+        chrome.tabs.create({ 'url': extension_page + "#" + info.selectionText }, function (tab) { });
     }
+}
+
+chrome.contextMenus.onClicked.addListener(onClick);
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.contextMenus.create({
+        title: 'Open in Native MPEG-Dash + HLS Playback',
+        contexts: ['link'],
+        id: 'link',
+        targetUrlPatterns: [
+            '*://*/Manifest*',
+            '*://*/*.mpd*',
+            '*://*/*.m3u*',
+            '*://*/*.m3u8*'
+        ]
+    });
+
+    chrome.contextMenus.create({
+        title: "Open in Native MPEG-Dash + HLS Playback", 
+        contexts:["selection"],
+        id: 'link-text'
+    });
 });
 
-chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: rules.map(r => r.id),
-    addRules: rules,
-});
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//     if (request == "getState") {
+//         sendResponse(enabled);
+//         return;
+//     }
+//       
+//     enabled = request == "ENABLE";
+//     enabled ? chrome.action.setIcon({path: "assets/icon128.png" }) : chrome.action.setIcon({ path:"assets/icon128grey.png" });
+// 
+//     if(true === enabled) {
+//         chrome.declarativeNetRequest.updateDynamicRules({
+//             removeRuleIds: rules.map(r => r.id),
+//             addRules: rules,
+//         });
+//     } else {
+//         chrome.declarativeNetRequest.updateDynamicRules(chrome.declarativeNetRequest.updateDynamicRules({
+//             removeRuleIds: rules.map(r => r.id)
+//         }));
+//     }
+// });
+// 
+// chrome.declarativeNetRequest.updateDynamicRules({
+//     removeRuleIds: rules.map(r => r.id),
+//     addRules: rules,
+// });
